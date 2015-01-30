6ce4fa43da87a6d18a84cbd8c3c5067183a25a51
Guilherme Vieira <super.driver.512@gmail.com>
2015-01-30 03:23:14 -0200

    Implement step 4's HTTP phone list loading.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,19 +1,14 @@
 var myAngle = angular.module('myAngle', []);
 
 myAngle.controller (
-	'PhoneListCtl', function($scope) {
-		$scope.phones = [
-			{
-				name: 'Phone A'
-				, description: 'A hypothetical phone represented by the letter A.'
-				, age: 2
-			}
-			, {
-				name: 'Phone B'
-				, description: 'A hypothetical phone represented by the letter B.'
-				, age: 1
-			}
-		];
+	'PhoneListCtl', function($scope, $http) {
+		$http.get('phones/phones.json')
+			.success (
+				function(data) {
+					$scope.phones = data;
+				}
+			)
+		;
 		$scope.order = 'age';
 	}
 );
