5d28c102d40f573d30bdafe5d9b12beb6a21398f
Guilherme Vieira <super.driver.512@gmail.com>
2015-01-30 03:24:53 -0200

    refactor: Declare PhoneListCtl dependencies using inline notation, as described in step 4.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,14 +1,18 @@
 var myAngle = angular.module('myAngle', []);
 
 myAngle.controller (
-	'PhoneListCtl', function($scope, $http) {
-		$http.get('phones/phones.json')
-			.success (
-				function(data) {
-					$scope.phones = data;
-				}
-			)
-		;
-		$scope.order = 'age';
-	}
+	'PhoneListCtl', [
+		'$scope'
+		, '$http'
+		, function($scope, $http) {
+			$http.get('phones/phones.json')
+				.success (
+					function(data) {
+						$scope.phones = data;
+					}
+				)
+			;
+			$scope.order = 'age';
+		}
+	]
 );
